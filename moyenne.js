c49fb805822ec1fb7d7ac035c49859795520da6a
aurelie <lieange1326@outlook.com>
2025-02-13 15:18:23 +0100

    secondary live

---
+++ moyenne.js
@@ -0,0 +1,65 @@
+console.log('test');
+
+
+// moyenne de........
+
+const notes= [10,15,20,0];
+let total=0;
+
+for(let element of notes){
+ total += element ;
+console.log(total);
+}
+
+const moyenne = total/notes.length;
+console.log("la moyenne est de ; "+ moyenne);
+
+if(moyenne>=15 & moyenne<=20){
+    console.log("votre moyenne est de "+ moyenne+ " toutes nos félicitations");
+}else if(moyenne>=10 & moyenne <15){
+    console.log( "votre moyenne est de "+ moyenne+ " tous nos encouragements")
+}else if(moyenne>=8 & moyenne<10){
+    console.log( "votre moyenne est de "+ moyenne+ " continuez votre progression")
+}else if(moyenne<8 & moyenne>=0){
+    console.log( "votre moyenne est de "+ moyenne+ " vous devez vous ressaisir")
+}else{
+    console.log('erreur dans le calcul de votre moyenne')
+};
+
+
+console.log("--------------------");
+//...........................................................
+ function calculMoyenne(note,prenom){
+
+    let total=0;
+
+    for(let element of note){
+        total += element ;
+       //console.log(total);
+       }
+       
+       const moyenne = total/notes.length;
+       console.log("la moyenne est de ; "+ moyenne);
+       
+       if(moyenne>=15 & moyenne<=20){
+           console.log(prenom +" votre moyenne est de "+ moyenne+ " toutes nos félicitations");
+       }else if(moyenne>=10 & moyenne <15){
+           console.log( prenom +" votre moyenne est de "+ moyenne+ " tous nos encouragements")
+       }else if(moyenne>=8 & moyenne<10){
+           console.log( prenom +" votre moyenne est de "+ moyenne+ " continuez votre progression")
+       }else if(moyenne<8 & moyenne>=0){
+           console.log(prenom + " votre moyenne est de "+ moyenne+ " vous devez vous ressaisir")
+       }else{
+           console.log('erreur dans le calcul de votre moyenne')
+       };
+
+}
+
+const notesAure= [10,15,20,0];
+const notesLili= [15,15,20,20];
+
+
+
+calculMoyenne(notesAure,"Aure");
+console.log("--------------------");
+calculMoyenne(notesLili,"Lili");
--- script.js
+++ script.js
@@ -19,3 +19,50 @@ prenom.style.background ="#b2b2b2";
 
 //style en css pour ajouter ou changer MAIS CSS dans fichier scc
 nom.classList.add('test-class');
+
+
+const notes= [10,15,20,5];
+console.log(notes);
+
+const fruits=["pomme","poire","fraise"];
+//console.log(fruits);
+console.log("je mange le fruit: "+ fruits[1])
+
+console.log("--------------------");
+//.................... FOR   avec compterur  on utilise l'index........................
+
+// de counter  0    a counter max tableau    de 1 en 1 
+for(let counter=0; counter<fruits.length ; counter++){
+   // console.log(counter);
+  console.log("je mange le fruit:"+fruits[counter]+"!");
+}
+
+console.log("--------------------");
+//...............................for of  on utilise la valeur.............................
+
+for(let element of fruits){
+ console.log("je mange le fruit: "+ element +"!");
+}
+
+console.log("--------------------");
+//.......................forEach. boucle sur des element html................
+
+fruits.forEach((element) =>{
+    console.log("je mange le fruit: "+ element+"!") ;
+});
+
+console.log("--------------------");
+//....................... Séléction des élément dans le html
+
+let formgroup=document.querySelectorAll('.form-group');//renvoie une collection
+//console.log(formgroup);NodeList [ div.form-group, div.form-group ]
+
+//passer la collection en tableaux
+Array.from(formgroup).forEach((element)=>{
+    console.log(element);//affiche chaque element de formgroup
+    //element.remove() :pour supprimer les deux éléments
+})
+
+ 
+
+
